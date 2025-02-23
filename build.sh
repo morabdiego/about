49a9a1912d98b9b68afc966335260a50a584c3c8
morabdiego <morabdiego@gmail.com>
2025-02-23 14:59:01 +0000

    update deploy for github pages

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ source .venv/bin/activate
 pip install -r requirements.txt
 reflex init
 reflex export --frontend-only
-rm -rf public
-unzip -o frontend.zip -d public
+rm -rf docs
+unzip -o frontend.zip -d docs/
 rm frontend.zip
 deactivate
