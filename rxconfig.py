5b19e4d103b9985303da1434528f71a6313d1a9b
morabdiego <morabdiego@gmail.com>
2025-03-03 16:38:35 +0000

    CORS

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,9 @@ import reflex as rx
 
 config = rx.Config(
     app_name="about",
-    show_built_with_reflex=False
+    show_built_with_reflex=False,
+    cors_allowed_origins=[
+        "https://morabdiego-about.vercel.app/",
+        "http://localhost:3000"
+        ],
 )
