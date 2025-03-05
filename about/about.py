ac28d3aef1297e8f9cc61883a6fb0db55c786060
morabdiego <morabdiego@gmail.com>
2025-03-05 20:07:22 +0000

    act. md 1

--- about/about.py
+++ about/about.py
@@ -1,5 +1,8 @@
 import reflex as rx
 from about.pages.index import index
 from about.pages.publications import publications
+from about.api.api import hello
 
 app = rx.App()
+
+app.api.add_api_route("/hello", hello) # Add an API route test
